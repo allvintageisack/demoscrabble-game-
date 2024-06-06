45c3764209350cb3043f15a3f47720518d99ac1f
leemungai <mrmungai@yahoo.com>
2024-06-06 19:09:40 +0200

    special scoring squares

--- board_and_management.py
+++
@@ -1,6 +0,0 @@
-class Board:
-    def __init__(self):
-        self.board = [["   " for _ in range(15)] for _ in range(15)] 
-        self.add_board()
-        
-        #Added Board class with constructor to initialize the game board
